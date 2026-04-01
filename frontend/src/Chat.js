696917a95944a55ec74e5779bd7c6a6a69921fbd
KandulaHemasri <hemasrikandula801@gmail.com>
2026-04-01 12:23:32 +0530

    updated

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://live-chat-dklc.onrender.com");
 
 function Chat() {
   const [name, setName] = useState("");
